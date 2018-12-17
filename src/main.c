f421cc061e7b006d6c1487ca82c07a09eeaef567
jeff.jbarbosa <jefferson.jaderbarbosa@gmail.com>
2018-12-16 22:40:25 -0200

    adicionando maquina de estados no main.c

--- src/main.c
+++ src/main.c
@@ -13,6 +13,38 @@
 
 uint8_t rx_pkg[16],i;
 
+void f_stateA(void);
+void f_stateB(void);
+void f_stateC(void);
+void f_stateD(void);
+
+/* Definição dos estados */
+typedef enum {
+	STATE_A,
+	STATE_B,
+	STATE_C,
+	STATE_D,
+	NUM_STATES
+} state_t;
+
+/* Definição da estrutura mantenedora do vetor de estados */
+typedef struct {
+	state_t myState;
+	void (*func)(void);
+}fsm_t;
+
+/* Mapeamento entre estado e funções */
+fsm_t myFSM[] = {
+	{ STATE_A, f_stateA },
+	{ STATE_B, f_stateB },
+	{ STATE_C, f_stateC },
+	{ STATE_D, f_stateD },
+
+};
+
+/* Estado atual */
+volatile state_t curr_state = STATE_A;
+
 int main(void)
 {	
 	//USART_Init(B9600);
@@ -22,12 +54,55 @@ int main(void)
 
     while (1) 
     {	
-		
+		// chamada da funcao referente ao estado atual da FSM
+		(*myFSM[curr_state].func)();
     }
 }
 
+// OBS: escolher qual interrupcao deve mudar o estado atual da FSM
 ISR(USART_RX_vect)
 {	
 	for (i=0; i < 8;i++)
 		rx_pkg[i] = USART_rx();
+	
+	// switch case para mudar o estado atual da FSM
+	switch (curr_state)
+	{
+		case STATE_A:
+		curr_state = STATE_B;
+		break;
+		
+		case STATE_B:
+		curr_state = STATE_C;
+		break;
+		
+		case STATE_C:
+		curr_state = STATE_D;
+		break;
+		
+		case STATE_D:
+		curr_state = STATE_A;
+		break;
+		
+		default:
+		break;
+	}
+	
+}
+
+// implementacao das funcoes da fsm
+void f_stateA(void){
+	// implementar o que deve ser feito nesse estado
+}
+
+void f_stateB(void){
+	// implementar o que deve ser feito nesse estado
+}
+
+void f_stateC(void){
+	// implementar o que deve ser feito nesse estado
+}
+
+void f_stateD(void){
+	// implementar o que deve ser feito nesse estado
 }

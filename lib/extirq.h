a9cb78001e615122516ae0eb3d12b41e0320272a
t4v0 <gugvfranca@gmail.com>
2018-12-18 08:08:05 -0200

    Correções da biblioteca extirq

--- lib/extirq.h
+++ lib/extirq.h
@@ -8,8 +8,6 @@
 #include <avr/interrupt.h>
 #include "../lib/bits.h"
 
-//void irq_hardware_init(uint8_t pin_rows, uint8_t * pin_change);
 void int0_hardware_init();
-uint8_t interrupt_int0();
 
 #endif
